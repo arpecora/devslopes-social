b244e2032f3d68e61544e118dbf5d71e7bd31883
Anthony Pecoraro <anthonypecoraro@Anthonys-MacBook-Air.local>
2017-06-04 15:59:02 -0400

    committed email auth

--- devslopes-social/SignInVC.swift
+++ devslopes-social/SignInVC.swift
@@ -7,18 +7,52 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInVC: UIViewController {
 
+    @IBOutlet weak var emailField: FancyField!
+    @IBOutlet weak var passwordField: FancyField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    func firebaseAuth( credential: AuthCredential) {
+        Auth.auth().signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("Unable to authenticate with Firebase")
+            } else {
+                print("Successfully authenticated with Firebase")
+            }
+        })
     }
+    
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let pwd = passwordField.text {
+            Auth.auth().signIn(withEmail: email, password: pwd, completion: {(user, error) in
+                if error == nil {
+                   print("Email user authenticated with Firebase")
+                } else {
+                    Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("Unable to authenticate with email Firebase")
+                        } else {
+                            print("Successfully authenticated new user with Firebase")
+                        }
+                     })
+                }
+                
+            })
+        }
+        
+    }
+
 
 
 }
